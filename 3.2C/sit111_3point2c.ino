950b4da9309ccf96785a0f68fa48b17fd55e4b81
Jason Vellucci <jason.vellucci@live.com>
2024-09-14 19:33:54 +1000

    Update sit111_3point2c.ino

--- 3.2C/sit111_3point2c.ino
+++ 3.2C/sit111_3point2c.ino
@@ -1,5 +1,5 @@
 // Define LED pins
-const int LED_PINS[8]   = {2, 3, 4, 5, 6, 7, 8, 9};
+const int LED_PINS[8] = {2, 3, 4, 5, 6, 7, 8, 9};
 const int LED_PIN_COUNT = sizeof(LED_PINS) / sizeof(LED_PINS[0]); // The number of elements representing `LED_PINS`.
 
 void setup() {
