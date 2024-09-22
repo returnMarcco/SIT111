c23c2d4462e9d5237cb4a635b97fa8e9a6bcc914
Jason Vellucci <jason.vellucci@live.com>
2024-09-22 12:02:30 +1000

    Create sketch_sep22a.ino

---
+++ 3.4P/sketch_sep22a.ino
@@ -0,0 +1,19 @@
+/**
+* SIT111 - 3.4P | T2, 2024
+* Jason Mark Vellucci | SID: 221437402
+*/
+
+const int SENSOR_PIN = A0; // Analog input pin connected to the soil moisture sensor.
+
+void setup() {
+	Serial.begin(9600); // Initialize serial communication.
+}
+
+void loop() {
+  int sensor_value = analogRead(SENSOR_PIN); // Read the value from the sensor. This is the INPUT from the `Soil Moisture Sensor` > `Arduino`.
+  int moisture_level = map(sensor_value, 0, 1023, 100, 0); // Map it to a 0-100% range.
+  Serial.print("Moisture Level: ");
+  Serial.print(moisture_level);
+  Serial.println("%");
+  delay(1000); // `ms` delay between readings.
+}
